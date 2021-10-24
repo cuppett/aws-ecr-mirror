870eb92ee240b69f9d44ae6f8cdc67cd0d595592
Stephen Cuppett <steve@cuppett.com>
2021-10-24 08:29:05 -0400

    Pulling out ecr_login function into a helper

--- mirror.py
+++ mirror.py
@@ -1,35 +1,6 @@
-import base64
-import boto3
-import string
+import helpers
 import subprocess
 import sys
-
-
-def ecr_login(repo: string) -> int:
-    repo_parts = repository.split(".")
-    ecr = boto3.client("ecr", region_name=repo_parts[3])
-
-    # Logging into the repository
-    auth_token = ecr.get_authorization_token()
-    if auth_token is None or \
-            len(auth_token["authorizationData"]) == 0 or \
-            "authorizationToken" not in auth_token["authorizationData"][0]:
-        print("Failure fetching authorzation token.")
-        return 2
-    # The authorizationToken is a base64 encoded version of 'AWS:my_password_here'
-    auth_password = base64.b64decode(auth_token["authorizationData"][0]["authorizationToken"])[4:]
-
-    logging_in = \
-        subprocess.run(
-            ["skopeo", "login", "-u", "AWS", "--password-stdin", repo],
-            input=auth_password,
-            stderr=subprocess.STDOUT,
-        )
-
-    if logging_in.returncode != 0:
-        print("Failure logging in to " + repository)
-    return logging_in.returncode
-
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -48,7 +19,7 @@ if __name__ == '__main__':
                 if repository not in repositories:
                     # Login in uniquely
                     repositories.append(repository)
-                    rc = ecr_login(repository)
+                    rc = helpers.ecr_login(repository)
                     if rc > 0:
                         exit(rc)
 
